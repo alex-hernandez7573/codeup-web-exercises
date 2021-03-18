4c304c879889b4cd664b55b6abd467b06f731171
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-03-18 15:28:34 -0500

    completed the js with html

---
+++ JS/lecture.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//var message = "Howdy y'all";
+
+//originally was a varible set to console.log
+// var whatIsThisThen = console.log (message + "Hope ya.......")
+
+//ar whatIsThisThen = message + "Hope y'all are having a good day"
+
+//console.log (whatIsThisThen);
+
+//x = 3.14;
+//var x = 3.14;
+
+/*
+*
+*   I'm a block comment
+*
+*
+*
+* */
+
+//var favCheese = prompt(message: "what is your fav chees?");
+
+//console.log (favCheese + " is it");
+
+
+///var favCheese = prompt("What is your favorite cheese?");
+
+//var num = prompt("Gimme a number...");
+//console.log("User Entered: "+num+". That number plus 17 is: "+(num+17) + ".");
+
+//var canIGtoToysRUs= confirm("can I go to toys r us?");
+
+alert  ("WHAAAAaaaaaaa!");
+
