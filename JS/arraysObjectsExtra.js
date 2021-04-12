60265a01c6b77c59e74f3bc7c821d17be38536e3
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-04-11 21:05:41 -0500

    finishing up extra work, arrays and objects

--- JS/arraysObjectsExtra.js
+++ JS/arraysObjectsExtra.js
@@ -3,10 +3,23 @@
 console.log ("testing...");
 
 /* # 1 Write a function, filterNumbers() that takes in an array of
- mixed data types and returns an array of only the numbers type in ascencding order.
-
+ mixed data types and returns an array of only the numbers type in ascending order.
 Example input: ["fred", true, 5, 3] Example output: [3, 5]
-* */
+
+
+*/
+
+var mixedDataTypes = [{Alice: name, age : 12},{David: name, age : 22},{Alex: name, age: 45}];
+
+function filterAgeAscending () {
+    mixedDataTypes.sort(name.age);
+    console.log(mixedDataTypes)
+}
+filterAgeAscending();
+
+
+
+// space
 var charcterArray =[{
     firstName: "test",
         stats: {
@@ -63,7 +76,7 @@ dogArray.forEach(function (doggyInfo){
 // array of car objects
 // and sets the boolean properties of isDirty to false.
 
-let carWashArray = [
+const carWashArray = [
     {
         make : "Ford",
         color : "White",
@@ -87,10 +100,35 @@ let carWashArray = [
     }
 ];
 
-//
+console.log ("Checking if dirty, if so change to clean")
+function carWashFunction (index) {
+    carWashArray.forEach((make, i) => make.isDirty = index === i);
+}
+carWashFunction(0-4)
+console.log(carWashArray);
+
+/* #4 Write a function, adminList() that takes in an array of
+ user objects and returns a count of
+all admins based on the isAdmin property. Refactor to return an
+ array of admin-only user emails.
+ Refactor again to return an array of user objects that are admins.*/
 
 
+const adminArray = [{
+    isAdmin: true,
+    email: 'david@email.com'
+},{
+    isAdmin: true,
+    email: 'alex@mail.com'
+},{
+    isAdmin: false,
+    email: 'sebas@email.com'
+}];
 
+// function showOnlyAdmin () {
+//     adminArray.forEach();
+// }
+// showOnlyAdmin()
 
 
 
