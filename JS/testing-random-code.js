5116acf09eee1441fbad31194fd3bdcb941ea10b
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-05-24 09:30:18 -0500

    making sure I'm up to date  with everything

---
+++ JS/testing-random-code.js
@@ -0,0 +1,12 @@
+"use strict"
+var logID = 'log',
+    log = $('<div id="'+logID+'"></div>');
+$('body').append(log);
+$('[type*="radio"]').change(function () {
+    var me = $(this);
+    log.html(me.attr('value'));
+});
+
+console.log(logID + " :: " + log)
+
+
